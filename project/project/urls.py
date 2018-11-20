90d1c604ba87ce044e453dbb6f87179ce414f8b7
q897586834 <897586834@qq.com>
2018-11-20 14:59:47 +0800

    页面

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path,include
+from django.urls import path, include
+from django.views.generic import TemplateView
 import xadmin
+from users.views import login
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', include('MxOnline.urls', namespace='MxOnline')),
+    path('index/', TemplateView.as_view(template_name="index.html"),name="index"),
+
+    path('login/', login, name="login"),
+
 ]
